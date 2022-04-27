a30aafd76128058aa763440a970fe64b36524db8
estherzzerpa <esterzerpa201@gmail.com>
2022-04-27 20:22:45 -0300

    bag caracter

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const btnMenu = document.querySelector(".bars__efect")
+// const menuDesplegable = document.querySelector(".container__opciones")
+
+// function menuHamburguesa(){
+//     menuDesplegable.style.display = "block"    
+// }
+
+
+// btnMenu.addEventListener("click", menuHamburguesa)
