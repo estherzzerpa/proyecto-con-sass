0811bdb57406d8347e0db8f18be7882927d7ea93
estherzzerpa <esterzerpa201@gmail.com>
2022-04-27 23:31:32 -0300

    menu hamburguesa js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-// const btnMenu = document.querySelector(".bars__efect")
-// const menuDesplegable = document.querySelector(".container__opciones")
+document.querySelector(".bars__efect").addEventListener("click", activeMenu)
+const menuDesplegable = document.querySelector(".container__opciones")
 
-// function menuHamburguesa(){
-//     menuDesplegable.style.display = "block"    
-// }
+const line1__bars = document.querySelector(".line1__bars-menu")
+const line2__bars = document.querySelector(".line2__bars-menu")
+const line3__bars = document.querySelector(".line3__bars-menu")
 
+function activeMenu(){
+    line1__bars.classList.toggle("activeline1__bars-menu")
+    line2__bars.classList.toggle("activeline2__bars-menu")
+    line3__bars.classList.toggle("activeline3__bars-menu")
+    menuDesplegable.classList.toggle("menu_active")
+}
 
-// btnMenu.addEventListener("click", menuHamburguesa)
